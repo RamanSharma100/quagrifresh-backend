a5dc252246809973244820e47240e1d16fd852da
Raman Sharma <rs9463194297@gmail.com>
2022-10-11 21:32:24 +0530

    initialize project

---
+++ server.js
@@ -0,0 +1,38 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+
+const app = express();
+
+// add morgan
+app.use(morgan("dev"));
+
+// add cors
+app.use(cors());
+
+// add body parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// add routes
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message:
+      "Welcome to the Grasslanders Network Eccomerce website's official server",
+  });
+});
+
+// add error handler
+app.use((err, req, res, next) => {
+  res.status(500).json({ message: err.message });
+});
+
+// add port
+const PORT = process.env.PORT || 5000;
+
+// add listener
+app.listen(PORT, () => {
+  console.log(`Development server is running on http://localhost:${PORT}`);
+});
